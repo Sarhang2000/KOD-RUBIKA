20288a94ad5441b4b647d316729f7d720918c05a
Sarhang2000 <waaw1387@gmail.com>
2024-08-03 04:09:52 +0330

    Add files via upload

---
+++ KOD-𝐒𝐀𝐑𝐇𝐀𝐍𝐆.py
@@ -0,0 +1,238 @@
+from os import system
+from time import sleep
+from colorama import Fore
+import sys
+
+system("clear")
+
+bnr = (Fore.LIGHTGREEN_EX+f"""
+
+   ID_Telegram = @SARHANG_HRX
+  
+   ID_Channel-telegram = @SARHANG_HRX
+
+
+
+
+____    _    ____  _   _    _    _   _  ____
+/ ___|  / \  |  _ \| | | |  / \  | \ | |/ ___|
+\___ \ / _ \ | |_) | |_| | / _ \ |  \| | |  _
+ ___) / ___ \|  _ <|  _  |/ ___ \| |\  | |_| |
+|____/_/   \_\_| \_\_| |_/_/   \_\_| \_|\____|
+
+
+
+
+""")
+for i in bnr:
+  sys.stdout.write(i)
+  sys.stdout.flush()
+  sleep(0.009)
+print("")
+name = input(Fore.LIGHTYELLOW_EX+"""
+(1) DSceapt
+(2) Sites sensitive
+(3) Bug
+(4) bug Servery
+(5) bug Numerical
+(6) bug Spam
+(7) bug Fake Account
+(8) bug Violence
+(9) bug Child Abuse
+(10) bug Pornography
+(11) Virus
+(12) Virus github
+(13) Link virus
+(14) Algoritm 
+(15) Site hack
+(16) Phishing
+(17) Sensitive translation
+(18) Sensitive words
+(19) Sensitive numbers
+(20) Sensitive Ping
+(21) Ter-af/am
+(22) Destructive linux
+(23) Destructive malware
+(24) Destructive ip
+(25) Destructive incriminating
+(26) Destructive Error
+(27) Destructive 404
+(28) Destructive githuby
+(29) Destructive normal
+(30) Site sex
+(31) Site gun
+(32) Site satan 
+(33) Site darkweb
+(34) Site red room
+(35) Site hack rubika
+(36) locky 
+(37) Zag virus
+(38) Sensitizer channel
+(39) Sensitizer account
+(40) Sensitizer group
+Select the number: """)
+print("")
+
+if name == "1":
+  print(Fore.LIGHTRED_EX+"http://radio-tacklorix.xlphp.net  |  http://dxprit-sxs-support-bot-hsck-fil.phpnet.us/fuk.sxs.photos.html   |  http://dxprit-sxs-support-bot-hsck-fil.phpnet.us/fuk.sxs.photos.html")
+  print("")
+
+if name == "2":
+  print(Fore.LIGHTRED_EX+"https://codepen.io/AlainBarrios/pen/pmZveP   |   http://maven.apache.org/xsd/maven-4.0.0.xsd   |   https://media.ccc.de/v/22C3-513-en-military_intelligence   |   http://vxvault.net/URL_List.php   |   http://maven.apache.org/xsd/maven-4.0.0.xsd   |   https://thenextweb.com/security/2017/03/27/oneplus-3-3t-malicious-charger-hack/#.tnw_fHHwquHT   |   https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt   |   https://secure.dshield.org/feeds/suspiciousdomains_Low.txt   |  https://www.lmgsecurity.com/5-dark-web-trends-in-2023/   |    https://www.globalsecuritymag.com/The-dark-web-has-a-alarming-2-7-million-daily-users-as-of-April-2023.html   |   https://ipfs.eth.aragon.network/ipfs/bafybeicli5rykqzju75bhact3z3xwhmdvx537w5jpllw56ja3jp7nthocy/nnooddvneng.html")
+  print("")
+  
+if name == "3":
+  print(Fore.LIGHTRED_EX+"Report.py(/CT,46.702,11.67C46.861,11.67,u0f44_99999999-7777777-/Akbaknkknnkq/-YftTkS-12233.4<Viru3>Badge_bug?php?/)=/Yftt15k/-17k/-18k/-20k/-30k/-40k/-50k/-70k/-80k/-85k/-Spam%100.ki\£Reporting/£Filtering\GHor.5.106.8.151.hfdjsh.122334.HYA=/@SupportBot<Report>bug.py   |   CT,6065,GH.Yi.ae,8085,0000.4(<Report>)ip+5.106.8.151,bug-server+81.28.49.29,fata.ir(<Filtering_Account>)FR.99977.m.m.M.99999999.4(<Server.of&on>)FHOM,0000.4,KpD.mn.m.c-01.00.02.000.00.02.01.000.01.000.00.02.00.01.000-tor(<fyll-ter.am/Af.af.af>)Sf.sf.sf/-43yjiig-/Af.af.af/-Permission-/Am.am.am")
+  print("")
+  
+if name == "4":
+  print(Fore.LIGHTRED_EX+"http://Server.onilline.oflline.bg/rubika+5.106.8.151/bug-on.of-server")
+  print("")
+  
+if name == "5":
+  print(Fore.LIGHTRED_EX+"https://server.Error/1.2.2.3.3.4/killd//bug-404-error//YftT/8.1.2.8.4.9.2.9.1.0.1.0.3.4.3.4.1.0.1.0.3.4.3.5.1.4.1.0.173.1.3.3.3.4.2.0.6.3.9.1.5.3/-Badge_99999999_Null_777777_999.7g.m.f.gk.f.a.mn.p.w.l.o.Yi.kmni_81.28.49.29.XXX.10.10.34.34.Phishing.10.10.34.35.Hack.141.0.173.133.Bet.13.107.246.35.Ping.3.33.139.32.Cyberpolice.34.206.39.153//-fyll-ter.Af.Sf.Am/-sf.af.am.sf.af.am.sf.am.af")
+  print("")
+  
+if name == "6":
+  print(Fore.LIGHTRED_EX+"https://Spam.rubika.ir/Spam%100/#c=b0Y0a2cafbaf668e282d2dc02a1fe2a7(Spam_Account)...spam%100/=(target=❗️)=/yftt15k/-Spam_100.k                                 ❗️=id target")
+  print("")
+  
+if name == "7":
+  print(Fore.LIGHTRED_EX+"https://Fake.rubika.ir/Fake_Account/#c=b0Y0a2cafbaf668e282d2dc02a1fe2a7(Rerport_Accout)=/Fake&Phishing&rubika/=(target=❗️)Fake/Report/SupportBot               ❗️=id target")
+  print("")
+  
+if name == "8":
+  print(Fore.LIGHTRED_EX+"https://Violence.rubika.ir/Violence_Account/#c=b0Y0a2cafbaf668e282d2dc02a1fe2a7(Report_Violence-Account)*cutting_off_head*(target=❗️)Violence/Report/SupportBot               ❗️=id target")
+  print("")
+  
+if name == "9":
+  print(Fore.LIGHTRED_EX+"https://Child_Abuse.rubika.ir/Child_Abuse/#c=b0Y0a2cafbaf668e282d2dc02a1fe2a7(Report_Child-Abuse)=/YftTkS_rubika&Child-Abuse&fill(target=)Report/Child Abuse/SupportBot   ❗️=id target")
+  print("")
+  
+if name == "10":
+  print(Fore.LIGHTRED_EX+"https://Pornography.rubika.ir/Account_Pornography/#c=b0Y0a2cafbaf668e282d2dc02a1fe2a7(Repory_Pornography)=/¥ftt_xXx.Account.Pornography(target=❗️)Report=/Pornography/=SupportBot              ❗️=id target")
+  print("")
+  
+if name == "11":
+  print(Fore.LIGHTRED_EX+"R0B1KA</bug_Bug_bUg/>11.11.Viru3.12.12/page.viruses/-V-infec")
+  print("")
+  
+if name == "12":
+  print(Fore.LIGHTRED_EX+"https://github.com/jakobzhao/virus")
+  print("")
+  
+if name == "13":
+  print(Fore.LIGHTRED_EX+"https://tinyurl.com/uppdatesnew   |   http://ffmodmenu.page.link/Anti-V  |  http://ffmodmenu.page.link/LatestMODMENU  |  https://bit.ly/3fX8ljZ  |  https://bit.ly/3ild93L")
+  print("")
+  
+if name == "14":
+  print(Fore.LIGHTRED_EX+"https://Support.bot/1.3.5.4/gk//DarkWeb-000-tor//15k/8.1.2.8.4.9.2.9.1.4.1.0.1.73.1.3.4.2.0.6.3.9.1.5.3.3.3.1.0.1.0.3.Kmni.4.3.5.1.3.1.0.7.2.4.6.3.5.1.0.1.0.3.4.3.4//违反规则//3.4.2.0.6.3.9.1.5.3/-Cyberpolice.gov.ir")
+  print("")
+  
+if name == "15":
+  print(Fore.LIGHTRED_EX+"https://noobhacktube.com")
+  print("")
+  
+if name == "16":
+  print(Fore.LIGHTRED_EX+"http://wiki.aa419.org/index.php/Main_Page")
+  print("")
+  
+if name == "17":
+  print(Fore.LIGHTRED_EX+"Report.rubika.ir/=The_account_has_violations=(#Pornography:Трансляція-порнографічного-контенту)&(#Spam:亵渎伊斯兰教、哈梅内伊和哈吉·卡西姆)&(#Violence:Погрози-та-надсилання-насильницького-контенту-на-Rubika)")
+  print("")
+  
+if name == "18":
+  print(Fore.LIGHTRED_EX+"yf113k❗️b2n❗️dadu❗️15k❗️negocFilter❗️Aiat_alah❗️gigfaedin❗️flickr❗️deviantart❗️polpy❗️yft❗️yfk❗️CFh❗️bg_json❗️yat3❗️b2n❗️Akbaknkknnkq❗️$¥flpom❗️YtTk❗️kidll❗️rm-rf/❗️flickr❗️q.C.rd❗️af❗️ae❗️ax❗️.0×C4,0×100,0×❗️bing❗️31d93LZ❗️3ild93L❗️freenom")
+  print("")
+  
+if name == "19":
+  print(Fore.LIGHTRED_EX+"+81.28.49.29❌+98❌+999❌77777.7❌999999999❌+5.106.8.151❌9999-77❌77❌999❌1.0.3.0❌88888888❌78ter❌909❌10.01❌01.00.000❌0x0❌000-tor❌0000.4❌15k❌%100❌000❌8085❌404❌5.3.5.30❌6065❌99999999❌777777❌4.5.4.9❌12233.4❌4444❌122334❌12233-4❌3.4.2.4❌1024❌(2.40.0)❌6.4-2❌3.4.4-1❌2.10.4❌1.5.5❌16.0.1-1❌(6.4-2)❌(2.10.4)❌(16.0.1-1)❌100% (917/917), done.❌100% (1499/1499)❌100% (424/424)❌100% (219/219)")
+  print("")
+  
+if name == "20":
+  print(Fore.LIGHTRED_EX+"khamenei+(10.10.34.35)   |   fraja+(86.105.245.69)   |   satan+(3.33.152.147)   |   cyberpolice+(81.28.49.29)   |   fata+(208.91.197.27)    |    phishing+(3.33.139.32)    |    hack+(34.206.39.153)    |    bet+(10.10.34.34)    |    rubika+(5.106.8.151)    |    xxx+(10.10.34.35)")
+  print("")
+  
+if name == "21":
+  print(Fore.LIGHTRED_EX+"bug?fyll-ter.Sf.Am.Af.Jg/-sf.am.af.jg.sf.am.af.jg.sf.am.af.jg.sf.am.af.jg.sf.am.af.jg.sf.am.af.jg.sf.am.af.jg.sf.am.af.jg.sf.am.af.jg.af")
+  print("")
+  
+if name == "22":
+  print(Fore.LIGHTRED_EX+"https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip")
+  print("")
+  
+if name == "23":
+  print(Fore.LIGHTRED_EX+"http://www.tekdefense.com/downloads/malware-samples/KUNKRNMALWARESAMPLE.rar")
+  print("")
+  
+if name == "24":
+  print(Fore.LIGHTRED_EX+"http://www.blocklist.de/lists/courierpop3.txt")
+  print("")
+  
+if name == "25":
+  print(Fore.LIGHTRED_EX+"https://sgts261.xyz/?channelCode=815")
+  print("")
+  
+if name == "26":
+  print(Fore.LIGHTRED_EX+"http://cdn.discordapp.com/attachments/879221762452226082/87922190902640")
+  print("")
+  
+if name == "27":
+  print(Fore.LIGHTRED_EX+"https://tendence.ru/articles/spam-chto-eto-takoe-prostymi-slovami-xxx/rubika.ir/rules")
+  print("")
+  
+if name == "28":
+  print(Fore.LIGHTRED_EX+"https://github.com/Ytthacker/xxaas/blob/main/XXX.txt")
+  print("")
+  
+if name == "29":
+  print(Fore.LIGHTRED_EX+"https://cigarettes-chemical-rid-numeric.trycloudflare.com  ~  https://bit.li/3ild93L/php-hack  ~  https://bit.li/3ild93L  ~  https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt  ~  https://bit.ly/3fX8ljZ/fil.rubika  ~  http://ffmodmenu.page.link/Anti-V   ~  https://v.3.0.9.yftt18k.in.xxx.com  ~  http://fil.sex.com/Distribution.immoral.content.com  ~  http://naturerleben-xhain.berlin/kieznaturkarte  ~  https://book.hacktricks.xyz  ~  http://zqktlwi4fecvo6ri.onion/wiki/index.php  ~  https://crash-bandicoot.info/enfejar/gambling-site  ~  https://dl.uploadgram.me/60bdffd3a2be4h  ~  https://cigarettes-chemical-rid-numeric.trycloudflare.com")
+  print("")
+  
+if name == "30":
+  print(Fore.LIGHTRED_EX+"https://m.me11r.com")
+  print("")
+  
+if name == "31":
+  print(Fore.LIGHTRED_EX+"https://www.gunsinternational.com")
+  print("")
+  
+if name == "32":
+  print(Fore.LIGHTRED_EX+"https://www.visitvictoria.com/regions/melbourne/eat-and-drink/bars/vv-naked-for-satan-rooftop")
+  print("")
+  
+if name == "33":
+  print(Fore.LIGHTRED_EX+"http://darkweb-sites.org   |  http://darkweblive.net")
+  print("")
+  
+if name == "34":
+  print(Fore.LIGHTRED_EX+"https://redroomnyc.com")
+  print("")
+  
+if name == "35":
+  print(Fore.LIGHTRED_EX+"https://hamyargps.com/blog/remote-rubika-hack")
+  print("")
+  
+if name == "36":
+  print(Fore.LIGHTRED_EX+"rest_122334_filteri.com  |  reset-99977_rubika.org   |   ¥=/yftt15.ki/-17k/-18k/-yftt20.ki   |  ([/yfttk-12233.4]Account_has_Infractions_rubika.ir    |   Null_999999999_rubik_7777777_Infractions   |   rest_999-Account-Infractions_polpy.Spam%100   |   xnx.0x0/-Account_porno+141.0.173.133/BuG_xXx   |   reset_99999999.4_Filtering_Account.bg")
+  print("")
+  
+if name == "37":
+  print(Fore.LIGHTRED_EX+"https://viru3.org/+999=/http://ffmodmenu.page.link/LatestMODMENU/=send_page_virus_in_rubika.ir/V-infect")
+  print("")
+  
+if name == "38":
+  print(Fore.LIGHTRED_EX+"www.SupportBot<Report_Account>rest-99999999.4_polpy.org<YftTkS_15k_17k_18k_yftt20k_yftt.ki>Uft|-122334|-killd_888|-999999999.9|-ReportRubika|-YftTkA_6055_yfttk_15|-c_0x0.0x0.0x0|-Darkweb-000-tor|-termux.dev|-Filtering_Rubika|-999999999.7777777|-bug-3.5.4-php|-01.00.02.000.03.0000.09|-Erroe:-:404:-:id_not_found:-:Error:-:102:-:Virus<Spam%100>Obscenity_against_th_ holy_things_of_Khamenei_Haj-Qasim.<pornography>Publication_of_pornographic_content_in_Rubika.ir<target=□>u0f66_4094571909,1467031594<■>Yftt_15k/-17k/-18k/-20k/-YftTk.bh<Virus.ly/page.virus/V-infect>                  □=destructive at cyberpolice.gov.ir for id target                          ■=destructive at http://darkweb-sites.org  for id target")
+  print("")
+  
+if name == "39":
+  print(Fore.LIGHTRED_EX+"www.SupportBot<Report_Account>rest-99999999.4_polpy.org<YftTkS_15k_17k_18k_yftt20k_yftt.ki>Uft|-122334|-killd_888|-999999999.9|-ReportRubika|-YftTkA_6055_yfttk_15|-c_0x0.0x0.0x0|-Darkweb-000-tor|-termux.dev|-Filtering_Rubika|-999999999.7777777|-bug-3.5.4-php|-01.00.02.000.03.0000.09|-Erroe:-:404:-:id_not_found:-:Error:-:102:-:Virus<Spam%100>Obscenity_against_th_ holy_things_of_Khamenei_Haj-Qasim.<pornography>Publication_of_pornographic_content_in_Rubika.ir<target=❗️>u0f66_4094571909,1467031594<💥>Yftt_15k/-17k/-18k/-20k/-YftTk.bh<Virus.ly/page.virus/V-infect>                         ❗️=destructive at cyberpolice.gov.ir for id target                                      💥=destructive at http://darkweb-sites.org  for id target")
+  print("")
+  
+if name == "40":
+  print(Fore.LIGHTRED_EX+"www.Netreport.ir/(#Report=send_Porn-text-img-video-file_&_obscenity-text_to_islam_&_users-rubika)fyl-ter.Af/-af.af.af.af.af.af.af.af.af.af.af.af.af.af.af.af.af.af.af.af.af=/print('Group_anti=@SupportBot')fyll-ter.Am/am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am.am=/print('Group_anti=http://rubika.ir/rules')ip,5.106.8.151,Fm.f.a.k.p.Yi.k.w.r.f.a.c.mn.h.g.k.p.o.ae,5.86ZM39.965,4.523C39.764,4.322</@SupportBot[Report]Check.ir\>")
+  print("")
+
+print("Coded by > SARHANG - HRX")
+print("")
+
